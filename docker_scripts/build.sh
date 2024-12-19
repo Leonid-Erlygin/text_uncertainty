86707f78611432519ab6c60a3f2684d8cd3c658e
leonid <erlygin.la@phystech.edu>
2024-12-19 17:51:47 +0000

    change COPY

--- docker_scripts/build.sh
+++ docker_scripts/build.sh
@@ -1,1 +1,1 @@
-docker build --build-arg="GID=${UID}" --build-arg="UID=${UID}" --build-arg="NAME=${USER}" -t ${USER}_$(basename $(dirname "$PWD")) ../docker_scripts
+docker build --build-arg="GID=${UID}" --build-arg="UID=${UID}" --build-arg="NAME=${USER}" -t ${USER}_$(basename $(dirname "$PWD")) .
