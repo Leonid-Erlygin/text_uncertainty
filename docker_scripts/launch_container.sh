2a12bba2f6fda3d8c230fbe9a5364e4620e89b32
leonid <erlygin.la@phystech.edu>
2025-01-14 08:28:29 +0000

    misc

--- docker_scripts/launch_container.sh
+++ docker_scripts/launch_container.sh
@@ -1,2 +1,2 @@
-docker run -d --shm-size=8g --memory=80g --cpus=40 --gpus '"device=0"' --user ${UID}:${UID} --name ${USER}_$(basename $(dirname "$PWD"))_dev_cont --rm -it --init -v $(dirname "$PWD"):/app ${USER}_$(basename $(dirname "$PWD")) bash
+docker run -d --shm-size=8g --memory=80g --cpus=40 --gpus '"device=2"' --user ${UID}:${UID} --name ${USER}_$(basename $(dirname "$PWD"))_dev_cont --rm -it --init -v $(dirname "$PWD"):/app ${USER}_$(basename $(dirname "$PWD")) bash
 docker exec ${USER}_$(basename $(dirname "$PWD"))_dev_cont pip install -e .
