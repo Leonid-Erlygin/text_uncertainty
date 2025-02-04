760dc259270b3b7e7b11d0263a3075096379e47d
leonid <erlygin.la@phystech.edu>
2025-02-04 17:25:07 +0000

    clear names

--- scripts/old_scripts/pred_emp_scf_cont.sh
+++
@@ -1,5 +0,0 @@
-python trainers/train.py predict \
- --config configs/train/train_scf.yaml \
- --ckpt_path=/app/model_weights/scf_base.ckpt \
-# --ckpt_path=/app/outputs/scf_uniform_batch/default_scf/epoch=14-step=218340.ckpt \
-
